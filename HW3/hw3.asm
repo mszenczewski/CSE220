1be1eb04ac5a8ed35fbda6942e0ecf5e83cd6187
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-02-24 21:48:11 -0500

    Update hw3.asm

--- HW3/hw3.asm
+++ HW3/hw3.asm
@@ -1,14 +1,3 @@
-# Michael Szenczewski
-# mszenczewski
-# 111267857
-
-#####################################################################
-############### DO NOT CREATE A .data SECTION! ######################
-############### DO NOT CREATE A .data SECTION! ######################
-############### DO NOT CREATE A .data SECTION! ######################
-##### ANY LINES BEGINNING .data WILL BE DELETED DURING GRADING! #####
-#####################################################################
-
 .text
 
 # Part I
@@ -910,10 +899,3 @@ decrypt:
 	addi $sp $sp 36 #registers times 4
 	
 	jr $ra
-
-#####################################################################
-############### DO NOT CREATE A .data SECTION! ######################
-############### DO NOT CREATE A .data SECTION! ######################
-############### DO NOT CREATE A .data SECTION! ######################
-##### ANY LINES BEGINNING .data WILL BE DELETED DURING GRADING! #####
-#####################################################################
